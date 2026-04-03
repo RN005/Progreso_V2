0ec8d5f96fdad3a06b8c5539c8dac018778d94c3
deep1190 <deepikagupta1190@gmail.com>
2026-04-03 18:34:31 +0530

    Session system + XP + analytics API

--- Progreso/backend/controllers/sessionController.js
+++ Progreso/backend/controllers/sessionController.js
@@ -1,5 +1,5 @@
 const Session = require("../models/session");
-const User = require("../models/User");
+const User = require("../models/user");
 const { calculateXP } = require("../utils/xpCalculator");
 
 // START SESSION
